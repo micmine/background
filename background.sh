5cc0f10b38c31294e66ae8423bc8d8bbf8fd47d6
micmine <micmine4@gmail.com>
2019-10-02 11:02:19 +0200

    Add pywal

--- background.sh
+++ background.sh
@@ -2,26 +2,29 @@
 path=~/Wallpapers
 
 setBackground() {
-        name=$(ls $path | dmenu -l 12)
-        fullpath=$path/$name
+  name=$(ls $path | dmenu -l 12)
+  fullpath=$path/$name
 
-        extension=$(echo "$name" | awk -F . '{print $NF}')
-        modifier=$(echo -e "no\nblur" | dmenu -l 12)
-        output="/tmp/wallpaper.$extension"
+  extension=$(echo "$name" | awk -F . '{print $NF}')
+  modifier=$(echo -e "no\nblur" | dmenu -l 2)
+  output="/tmp/wallpaper.$extension"
 
-        if [ "$modifier" == "no" ]; then
-                cp $fullpath $output
-        elif [ "$modifier" == "blur" ]; then
-                convert $fullpath -blur 0x8 $output
-        fi
+  if [ "$modifier" == "no" ]; then
+    cp $fullpath $output
+  elif [ "$modifier" == "blur" ]; then
+    convert $fullpath -blur 0x8 $output
+  fi
 
-        if [ "$XDG_CURRENT_DESKTOP" == "ubuntu:GNOME" ]; then
-                dconf write "/org/gnome/desktop/background/picture-uri" "'file:///$output'"
+  if [ "$XDG_CURRENT_DESKTOP" == "ubuntu:GNOME" ]; then
+    dconf write "/org/gnome/desktop/background/picture-uri" "'file:///$output'"
 	elif [ "$XDG_CURRENT_DESKTOP" == "GNOME" ]; then
 		dconf write "/org/gnome/desktop/background/picture-uri" "'file:///$output'"
-        else
-                feh --bg-scale $output
-        fi
+  else
+    feh --bg-scale $output
+    if [ -x "$(command -v wal)" ]; then
+      wal -i $fullpath -n
+    fi
+  fi
 }
 
 addBackground() {
