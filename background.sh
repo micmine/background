a2d495dbf89e988de8f579e3b6fde981ffe4590f
micmine <micmine4@gmail.com>
2020-03-23 16:15:57 +0100

    Add cli option

--- background.sh
+++ background.sh
@@ -1,37 +1,47 @@
 #!/bin/bash
 path=~/Wallpapers
-.cache/wal/colors.sh
+
+name=""
+fullpath=/
+modifier=".png"
 
 pywal() {
-  if [ -x "$(command -v wal)" ]; then
-    wal -i "$fullpath" -n
-  fi
-  ~/.config/glava/change_glava.sh
-  ~/.config/dunst/reload_dunst.sh
+	if [ -x "$(command -v wal)" ]; then
+		wal -i "$fullpath" -n > /dev/null 2> /dev/null
+
+	fi
+	~/.config/dunst/reload_dunst.sh > /dev/null 2> /dev/null
+	notify-send 'Changed background' $name --icon=dialog-information
+
+	~/.config/glava/change_glava.sh > /dev/null 2> /dev/null
+
+}
+
+uiSelect() {
+	name=$(ls "$path" | rofi -dmenu)
+	fullpath="$path/$name"
+	modifier=$(echo -e "no\\nblur" | rofi -dmenu )
 }
 
 setBackground() {
-  name=$(ls "$path" | rofi -dmenu)
-  fullpath="$path/$name"
+	extension=$(echo "$name" | awk -F . '{print $NF}')
+	output="/tmp/wallpaper.$extension"
 
-  extension=$(echo "$name" | awk -F . '{print $NF}')
-  modifier=$(echo -e "no\\nblur" | rofi -dmenu )
-  output="/tmp/wallpaper.$extension"
-
-  if [ "$modifier" == "no" ]; then
-    cp "$fullpath" "$output"
-  elif [ "$modifier" == "blur" ]; then
-    convert "$fullpath" -blur 0x8 "$output"
-  fi
-
-  if [ "$XDG_CURRENT_DESKTOP" == "ubuntu:GNOME" ]; then
-    dconf write "/org/gnome/desktop/background/picture-uri" "'file:///$output'"
+	if [ "$modifier" == "no" ]; then
+		cp "$fullpath" "$output"
+	elif [ "$modifier" == "blur" ]; then
+		convert "$fullpath" -blur 0x8 "$output"
+	fi
+	
+	if [ "$XDG_CURRENT_DESKTOP" == "ubuntu:GNOME" ]; then
+		dconf write "/org/gnome/desktop/background/picture-uri" "'file:///$output'"
 	elif [ "$XDG_CURRENT_DESKTOP" == "GNOME" ]; then
 		dconf write "/org/gnome/desktop/background/picture-uri" "'file:///$output'"
-  else
-    feh --bg-scale "$output"
-  fi
-  pywal
+	else
+		feh --bg-scale "$output"
+	fi
+  	
+	pywal
 }
 
 addBackground() {
@@ -41,12 +51,18 @@ addBackground() {
 	curl -o "$path/$name" "$url"
 }
 
-action=$(echo -e "set\\nadd\\nsetup" | rofi -dmenu)
-
-if [ "$action" == "set" ]; then
+if ! [ -z "$1" ]; then
+	name="$1"
+	fullpath="$name"	
+	modifier="no"
 	setBackground
-elif [ "$action" == "add" ]; then
-	addBackground
-elif [ "$action" == "setup" ]; then
-	apt-get install zenity dmenu feh xclip imagemagick
+else
+	action=$(echo -e "set\\nadd" | rofi -dmenu)
+	if [ "$action" == "set" ]; then
+		uiSelect
+		setBackground
+	elif [ "$action" == "add" ]; then
+		addBackground
+	fi
 fi
+
